2651ff5958adbef25229a5f8d63d686446c3c317
Thirathawat Chanakit <thirathawat996@gmail.com>
2024-12-01 18:06:18 +0700

    eslint

--- dsde_dashboard/eslint.config.js
+++ dsde_dashboard/eslint.config.js
@@ -1,18 +1,15 @@
-{
-    ('extends');
-    [
-        'next/core-web-vitals', // For Next.js
-        'eslint:recommended',
-        'plugin:@next/next/recommended',
-        'plugin:react/recommended',
-        'plugin:prettier/recommended',
+module.exports = {
+    extends: [
+      'next/core-web-vitals',            // Recommended Next.js rules
+      'eslint:recommended',
+      'plugin:@next/next/recommended',
+      'plugin:react/recommended',
+      'plugin:prettier/recommended'      // Integrates Prettier with ESLint
     ],
-        'plugins';
-    ['prettier'], 'rules';
-    {
-        ('prettier/prettier');
-        'error', // Marks Prettier issues as errors
-            'react/react-in-jsx-scope';
-        ('off'); // Next.js doesn't need React import in scope
+    plugins: ['prettier'],
+    rules: {
+      'prettier/prettier': 'error',      // Treat Prettier formatting issues as errors
+      'react/react-in-jsx-scope': 'off'  // Next.js doesn't require React imports in scope
     }
-}
+  };
+  
