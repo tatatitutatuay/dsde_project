ca53ea0366d2d464c722d3783ee50f617b28d4e3
Thirathawat Chanakit <thirathawat996@gmail.com>
2024-12-06 16:49:43 +0700

    backend: num keywords

--- model-backend/api/app.py
+++ model-backend/api/app.py
@@ -17,12 +17,16 @@ def home():
 def post_data():
     # Get the abstract text from the request
     abstract = request.get_json().get('abstract')
+    num_keywords = request.get_json().get('num_keywords')
+    
+    num_keywords = num_keywords if num_keywords else 5
     
     # Predict keywords from the abstract
-    keywords = predict_keywords_from_abstract(abstract, min_keywords=5)
+    keywords = predict_keywords_from_abstract(abstract, min_keywords=num_keywords)
     
     # Return the keywords as a JSON response
     return jsonify(keywords)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
