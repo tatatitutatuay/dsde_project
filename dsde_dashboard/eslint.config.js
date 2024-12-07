870a0be136a0143312a7056dc3f766eb622763d8
popo <yosita.rd@gmail.com>
2024-12-08 01:11:36 +0700

    dashboard: refactor ESLint configuration to use defineConfig and modern syntax

--- dsde_dashboard/eslint.config.js
+++ dsde_dashboard/eslint.config.js
@@ -1,14 +1,25 @@
-module.exports = {
-    extends: [
-        'next/core-web-vitals', // Recommended Next.js rules
-        'eslint:recommended',
-        'plugin:@next/next/recommended',
-        'plugin:react/recommended',
-        'plugin:prettier/recommended', // Integrates Prettier with ESLint
-    ],
-    plugins: ['prettier'],
-    rules: {
-        'prettier/prettier': 'error', // Treat Prettier formatting issues as errors
-        'react/react-in-jsx-scope': 'off', // Next.js doesn't require React imports in scope
+import { defineConfig } from 'eslint-define-config';
+
+export default defineConfig([
+    {
+        files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'], // Specify the files to lint
+        languageOptions: {
+            parserOptions: {
+                ecmaVersion: 2023,
+                sourceType: 'module',
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+        plugins: {
+            prettier: require('eslint-plugin-prettier'),
+            react: require('eslint-plugin-react'),
+            '@next/next': require('@next/eslint-plugin-next'),
+        },
+        rules: {
+            'prettier/prettier': 'error',
+            'react/react-in-jsx-scope': 'off',
+        },
     },
-};
+]);
