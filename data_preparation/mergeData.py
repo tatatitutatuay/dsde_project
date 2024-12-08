fb93705580b8cb02126caffca807551281ecfddf
Tippaphanun Kungwarnkanok <tip.kus48@gmail.com>
2024-12-08 21:41:29 +0700

    spark dataframe

--- data_preparation/mergeData.py
+++ data_preparation/mergeData.py
@@ -1,4 +1,11 @@
 import pandas as pd
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType
+import os
+import shutil
+
+# Initialize Spark session
+spark = SparkSession.builder.appName("Combine CSVs").getOrCreate()
 
 # combine two csv files
 df1 = pd.read_csv('data_preparation/web_scraping/filtered_output.csv')
@@ -6,7 +13,22 @@ df2 = pd.read_csv('data_preparation/given_data/data/data_noTHInAbstract.csv')
 
 df1 = df1.drop(columns=['country'])
 
-df = pd.concat([df1, df2], ignore_index=True)
+combined_df = pd.concat([df1, df2], ignore_index=True)
+
+schema = StructType([
+    StructField("abstract", StringType(), True),
+    StructField("keywords", StringType(), True),
+])
+
+df = spark.createDataFrame(combined_df, schema)
 
 # Save the merged data to a new CSV file
-df.to_csv('data_preparation/trainData.csv', index=False)
+output_dir = 'data_preparation/temp'
+single_file = 'data_preparation/trainData.csv'
+
+df.coalesce(1).write.csv(output_dir, header=True, mode='overwrite')
+
+for file_name in os.listdir(output_dir):
+    if file_name.endswith(".csv"):
+        shutil.move(os.path.join(output_dir, file_name), single_file)
+shutil.rmtree(output_dir)
